d59bdd3ec5ff0cb4bcadfa5b4a25f2e11afc6a9c
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-10-01 09:32:31 -0400

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,21 @@
+x = ["walk","bike","ebike","bus","car"]
+while True:
+    y = input("Enter a,b,c,d,e, to get a mode of transport.\n")
+    if y == "a":
+        y = x[0]
+    elif y == "b":
+        y = x[1]
+    elif y == "c":
+        y = x[2]
+    elif y == "d":
+        y = x[3]
+    elif y == "e":
+        y = x[4]
+    elif y == "x":
+        break
+    else:
+        print("Enter a,b,c,d,e only.")
+        continue
+    print(f"you picked {y}.")
+        
+
